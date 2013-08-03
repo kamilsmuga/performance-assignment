6ab2054d371f3591fb4a06ee021bfe0622a1b890
kamil <ksmuga@salesforce.com>
2013-08-03 18:54:54 +0100

    added jmeter graphs generation after each test

--- 3_perf_testing/auto/auto.sh
+++ 3_perf_testing/auto/auto.sh
@@ -30,12 +30,14 @@ function attach_visualvm () {
 # takes 2 arguments: jmeter plan location and output file location
 function start_remote_jmeter () {
 	echo "Starting remote test plan with jmeter"
+	echo "Preparing output dir $3 on $MASTER"
+	ssh ks@$MASTER "mkdir -p ${JMETER_DIR}/$3"
 	echo "Copying $1 to $MASTER"
-	scp $1 ks@$MASTER:$JMETER_DIR
+	scp $1 ks@$MASTER:$JMETER_DIR/$3
 	echo "Executing jmeter remote test plan on $MASTER"
-	ssh ks@$MASTER "cd ${JMETER_DIR} ; java -jar ApacheJMeter.jar -n -r -t $1 -l $2"
+	ssh ks@$MASTER "cd ${JMETER_DIR} ; java -jar ApacheJMeter.jar -n -r -t $3/$1 -l $3/$2"
 	echo "Copying back results from test plan" 
-	scp ks@$MASTER:${JMETER_DIR}/$2 $3
+	scp ks@$MASTER:${JMETER_DIR}/$3/$2 $3
 	echo "start_remote_jmeter() finished"
 }
 
@@ -57,7 +59,45 @@ function get_gc_logs () {
 }
 # generate graphs from jmeter results
 function generate_jmeter_graphs () {
-	ssh ks@$MASTER "cd ${JMETER_DIR}"
+	OUT=$JMETER_DIR/$2
+	echo "Generating ResponseTimesOverTime graph from results $1"
+	ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_response_times_over_time.png --input-jtl $OUT/$1 --plugin-type ResponseTimesOverTime --width 800 --height 600 "
+	echo "Generating Active Threads Over Time graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_active_threads_over_time.png --input-jtl $OUT/$1 --plugin-type ThreadsStateOverTime --width 800 --height 600 "
+	
+	echo "Generating BytesThroughputOverTime graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_bytes_throughput_over_time.png --input-jtl $OUT/$1 --plugin-type BytesThroughputOverTime --width 800 --height 600 "
+	
+	echo "Generating HitsPerSecond graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_hits_per_second.png --input-jtl $OUT/$1 --plugin-type HitsPerSecond --width 800 --height 600 "
+
+	echo "Generating LatenciesOverTime graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_latencies_over_time.png --input-jtl $OUT/$1 --plugin-type LatenciesOverTime --width 800 --height 600 "
+
+	echo "Generating ResponseTimesDistribution graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_response_times_distribution.png --input-jtl $OUT/$1 --plugin-type ResponseTimesDistribution --width 800 --height 600 "
+
+	echo "Generating ResponseTimesOverTime graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_response_times_over_time.png --input-jtl $OUT/$1 --plugin-type ResponseTimesOverTime --width 800 --height 600 "
+
+	echo "Generating ThroughputOverTime graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_throughput_over_time.png --input-jtl $OUT/$1 --plugin-type ThroughputOverTime --width 800 --height 600 "
+
+	echo "Generating ThroughputVsThreads graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_throughput_vs_threads.png --input-jtl $OUT/$1 --plugin-type ThroughputVsThreads --width 800 --height 600 "
+
+        echo "Generating TimesVsThreads graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_times_vs_threads.png --input-jtl $OUT/$1 --plugin-type TimesVsThreads --width 800 --height 600 "
+
+        echo "Generating TransactionsPerSecond graph from results $1"
+        ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-png $OUT/$1_transactions_per_second.png --input-jtl $OUT/$1 --plugin-type TransactionsPerSecond --width 800 --height 600 "
+
+	
+	echo "Generating summary report in CSV"
+	ssh ks@$MASTER "cd ${JMETER_DIR}/../lib/ext/ ; java -jar CMDRunner.jar --tool Reporter --generate-csv $OUT/$1_summary_report.csv --input-jtl $OUT/$1 --plugin-type AggregateReport"
+
+	echo "Copying generated graphs $1.png to $2"
+	scp ks@$MASTER:$OUT/*.png $2
 }
 
 # main loop function
@@ -72,6 +112,7 @@ function main () {
 	   # warmup jboss
 	   sleep 180
 	   start_remote_jmeter $f $f.out $OUT
+	   generate_jmeter_graphs $f.out $OUT
 	   stop_jboss
 	   get_gc_logs $OUT
     	   echo $f
@@ -88,6 +129,7 @@ function test () {
 	   get_jboss_pid
 	   attach_visualvm
 	   start_remote_jmeter $f $f.out $OUT
+	   generate_jmeter_graphs $f.out $OUT
 	   #stop_jboss
     	   #echo $f
 	done
